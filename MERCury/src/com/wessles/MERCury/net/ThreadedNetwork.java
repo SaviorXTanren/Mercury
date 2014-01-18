00c2ef424edafcb7c13f0188bca6de14d49aee6d
Toby Simone <pancakesimone@gmail.com>
2014-01-18 00:14:31 -0500

    Networking update #1
    Added most of networking. Using Kryonet.

---
+++ MERCury/src/com/wessles/MERCury/net/ThreadedNetwork.java
@@ -0,0 +1,38 @@
+package com.wessles.MERCury.net;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Listener.ThreadedListener;
+
+/**
+ * @from MERCury in com.wessles.MERCury.net
+ * @by opiop65
+ * @website www.wessles.com
+ * @license (C) Jan 17, 2014 www.wessles.com This file, and all others of the project 'MERCury' are licensed under WTFPL license. You can find the license itself at http://www.wtfpl.net/about/.
+ */
+
+public class ThreadedNetwork extends ThreadedListener{
+	
+	//All the methods in this class will be seperately threaded through the QueuedListener in the Kryonet library.
+
+	//Requires a Listener from the Kryonet package, probably need to update it to reference from the Network.java from the MERCury engine.
+	public ThreadedNetwork(Listener listener) {
+		super(listener);
+	}
+	
+	public void connected(Connection connection){
+	}
+	
+	//Called when a remote client is disconnected.
+	public void disconnected(Connection connection){
+	}
+	
+	//Called when an object is received from a remote client. This method will not block any threads by design.
+	public void received(Connection connection, Object object){
+	}
+
+	//Called when the connection is below the idle threshold.
+	public void idle(Connection connection){
+	}
+
+}
