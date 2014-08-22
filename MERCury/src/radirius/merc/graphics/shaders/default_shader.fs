8b63f8dc82425e9adac951f67f605f3475adb576
weslgames <wesley.laferriere@gmail.com>
2014-08-22 01:49:20 -0400

    Hopefully fixed skape's problem; downgraded default shader languages.

--- MERCury/src/radirius/merc/graphics/shaders/default_shader.fs
+++ MERCury/src/radirius/merc/graphics/shaders/default_shader.fs
@@ -1,9 +1,7 @@
-#version 130
-
 uniform sampler2D u_texture;
 
-in vec4 v_color;
-in vec2 v_texcoord;
+varying vec4 v_color;
+varying vec2 v_texcoord;
 
 void main() {
     vec4 tex = texture2D(u_texture, v_texcoord);
--- MERCury/src/radirius/merc/graphics/shaders/default_shader.vs
+++ MERCury/src/radirius/merc/graphics/shaders/default_shader.vs
@@ -1,7 +1,5 @@
-#version 130
-
-out vec4 v_color;
-out vec2 v_texcoord;
+varying vec4 v_color;
+varying vec2 v_texcoord;
 
 void main()
 {
